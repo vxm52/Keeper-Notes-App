f156ab62d989eaca5a25fcb334c02ef8de6abbe2
vxm52 <vaibhav.s.malla@gmail.com>
2021-03-18 16:26:17 -0400

    Added existing project files

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem) => {
+        return <Note title={noteItem.title} content={noteItem.content} />;
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
