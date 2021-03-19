db5f5c2e241ce871186be7b5b485f8eaf3bd07ba
vxm52 <vaibhav.s.malla@gmail.com>
2021-03-19 14:53:57 -0400

    Added Material-UI

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,17 +1,22 @@
 import React from "react";
+import DeleteIcon from "@material-ui/icons/Delete";
+import Zoom from "@material-ui/core/Zoom";
 
 function Note(props) {
-
   function handleClick() {
-    props.onDelete(props.id);   //Executes the deleteNote() function in App.jsx
+    props.onDelete(props.id); //Executes the deleteNote() function in App.jsx
   }
 
   return (
-    <div className="note">
-      <h1>{props.title}</h1>
-      <p>{props.content}</p>
-      <button onClick={handleClick}>DELETE</button>
-    </div>
+    <Zoom in={true}>
+      <div className="note">
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
+        <button onClick={handleClick}>
+          <DeleteIcon />
+        </button>
+      </div>
+    </Zoom>
   );
 }
 
